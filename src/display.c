db6e965f8606a082233f6aed08aef4916f1165d8
oliver-goeken <oliverdgoeken@gmail.com>
2026-01-21 16:25:19 -0800

    add comments

--- src/display.c
+++ src/display.c
@@ -1,9 +1,18 @@
 #include "display.h"
 
+/*
+ * @brief intializes ncurses
+ *
+ * @return 0 on success, -1 on error
+ *
+ * @details
+ * should only be called at the very start of using ncurses
+ */
 int display_init(){
 	WINDOW* w = initscr();
-	if (w == NULL)
+	if (w == NULL){
 		return -1;
+	}
 
 	cbreak();
 	noecho();
@@ -12,6 +21,14 @@ int display_init(){
 	return 0;
 }
 
+/*
+ * @brief terminates ncurses
+ *
+ * @return 0 on success, -1 on error
+ *
+ * @details
+ * only call when done with ncurses
+ */
 int display_kill(){
 	if (endwin() != OK){
 		return -1;
