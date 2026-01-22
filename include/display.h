d876e8e418956e55a5000ef32d4d9b628199efc1
oliver-goeken <oliverdgoeken@gmail.com>
2026-01-22 12:12:14 -0800

    add basic wrapper for ncurses window

--- include/display.h
+++ include/display.h
@@ -3,6 +3,23 @@
 
 #include <ncurses.h>
 
+typedef struct display_window_content_node {
+	struct display_window_content_node* NEXT;
+	struct display_window_content_node* PREV;
+} display_window_content_node;
+
+typedef struct display_window {
+	int start_x;
+	int start_y;
+	int width;
+	int height;
+
+	WINDOW* window;
+
+	display_window_content_node* content;
+	int content_offset;
+} display_window;
+
 /*
  * @brief intializes ncurses
  *
@@ -22,5 +39,15 @@ int display_init();
  * only call when done with ncurses
  */
 int display_terminate();
+
+display_window* display_create_window(int start_x, int start_y, int height, int width);
+int display_destroy_window(display_window* window);
+
+int display_resize_window(display_window* window, int new_width, int new_height);
+int display_move_window(display_window* winodw, int new_begin_x, int new_begin_y);
+
+int display_draw_window(display_window* window);
+
+int display_draw_window_contents(display_window* window);
 
 #endif
--- src/display.c
+++ src/display.c
@@ -1,13 +1,14 @@
 #include "display.h"
+#include <stdlib.h>
 
 int display_init(){
-	if (initscr() == NULL){
-		return -1;
-	}
+	initscr();
 
+	keypad(stdscr, TRUE);
+	nonl();
 	cbreak();
 	noecho();
-	keypad(stdscr, true);
+	curs_set(0);
 
 	return 0;
 }
@@ -16,6 +17,72 @@ int display_terminate(){
 	if (endwin() != OK){
 		return -1;
 	}
+
+	return 0;
+}
+
+display_window* display_create_window(int start_x, int start_y, int height, int width){
+	display_window* new_display_window = malloc(sizeof(display_window));
+
+	new_display_window->window = newwin(height, width, start_y, start_x);
+
+	new_display_window->start_x = start_x;
+	new_display_window->start_y = start_y;
+	new_display_window->height = height;
+	new_display_window->width = width;
+
+	new_display_window->content = NULL;
+
+	return new_display_window;
+}
+
+int display_destroy_window(display_window* window){
+	delwin(window->window);
+	free(window);
+
+	return 0;
+}
+
+int display_move_window(display_window* window, int new_begin_x, int new_begin_y){
+	window->start_x = new_begin_x;
+	window->start_y = new_begin_y;
+
+	wborder(window->window, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+	wrefresh(window->window);
+	delwin(window->window);
+
+	window->window = newwin(window->height, window->width, window->start_y, window->start_x);
+
+	display_draw_window(window);
+
+	return 0;
+}
+
+int display_resize_window(display_window* window, int new_width, int new_height){
+	window->width = new_width;
+	window->height = new_height;
+
+	wborder(window->window, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+	wrefresh(window->window);
+	delwin(window->window);
+
+	window->window = newwin(window->height, window->width, window->start_y, window->start_x);
+
+	display_draw_window(window);
+
+	return 0;
+}
+
+int display_draw_window(display_window* window){
+	display_draw_window_contents(window);
+
+	wrefresh(window->window);
+
+	return 0;
+}
+
+int display_draw_window_contents(display_window* window){
+	mvwprintw(window->window, 1, 1, "Hello, World!");
 
 	return 0;
 }
