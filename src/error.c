5d6f2a0dd5fa2b23f39f80154be3b76d03e1b3cd
oliver-goeken <oliverdgoeken@gmail.com>
2026-01-21 16:14:28 -0800

    added comments to error.

--- src/error.c
+++ src/error.c
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Exits with passed code, printing the provided filename and line number along with a given message to stderr
+// Takes:
+// int code: error code
+// char* msg: message to print to stderr
+// char* file: filename of error, provided by __FILE__ macro when calling
+// int line: line number of error, provided by __LINE__ macro when calling
+
 void throw_error(int code, char* msg, char* file, int line){
 	fprintf(stderr, "%s:%d -- %s\n", file, line, msg);
 	exit(code);
