7e910c897d9fffaf510643e5a74de93f7c4a5569
oliver-goeken <oliverdgoeken@gmail.com>
2026-01-21 17:26:01 -0800

    move more comments

--- include/error.h
+++ include/error.h
@@ -1,6 +1,19 @@
 #ifndef error_h
 #define error_h
 
+/*
+ * @brief Throws an error with the provided information
+ *
+ * @param code: error code to exit with
+ * @param msg: message to print to stderr on exiting
+ * @param file: filename where error occured (pass the __FILE__ macro)
+ * @param line: line number where error occured (pass the __LINE__ macro)
+ *
+ * @return nothing
+ *
+ * @details
+ * Exits with passed code, printing the provided filename and line number along with a given message to stderr
+ */
 void throw_error(int code, char* msg, char* file, int line);
 
 #endif
--- src/error.c
+++ src/error.c
@@ -2,19 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * @brief Throws an error with the provided information
- *
- * @param code: error code to exit with
- * @param msg: message to print to stderr on exiting
- * @param file: filename where error occured (pass the __FILE__ macro)
- * @param line: line number where error occured (pass the __LINE__ macro)
- *
- * @return nothing
- *
- * @details
- * Exits with passed code, printing the provided filename and line number along with a given message to stderr
- */
 void throw_error(int code, char* msg, char* file, int line){
 	fprintf(stderr, "%s:%d -- %s\n", file, line, msg);
 	exit(code);
