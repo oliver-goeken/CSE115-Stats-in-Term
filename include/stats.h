a3aba9bca0d3da8f9e201b29f51b7f015e3991ed
oliver-goeken <oliverdgoeken@gmail.com>
2026-03-02 12:34:48 -0800

    move sigint flag to header

--- include/stats.h
+++ include/stats.h
@@ -3,11 +3,16 @@
 
 #include "display.h"
 
+bool SIGINT_FLAG;
+
 // initializes everything
 void init();
 
 //terminates everything
 void terminate();
+
+// handle interrupt signal
+void handle_sigint();
 
 // function handler for the yes button on the quit screen
 void quit_yes_button_interact(display_content_node* content_node);
