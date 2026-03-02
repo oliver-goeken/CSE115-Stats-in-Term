1eb002cf6571608d83c58ad14ab605c27fc9d531
oliver-goeken <oliverdgoeken@gmail.com>
2026-03-01 19:05:55 -0800

    add error macros to logging

--- include/log.h
+++ include/log.h
@@ -1,8 +1,11 @@
 #ifndef LOG_H
 #define LOG_H
 
-#define log_msg(msg);  log_msg_detailed(__FILE__, __LINE__, msg);
-#define log_msg_f(format_msg, ...); log_message_f_detailed(__FILE__, __LINE__, format_msg, ...);
+#define log_msg(msg);  log_msg_detailed("", __FILE__, __LINE__, msg);
+#define log_err(msg);  log_msg_detailed("ERROR: ", __FILE__, __LINE__, msg);
+
+#define log_msg_f(format_msg, ...); log_msg_f_detailed("", __FILE__, __LINE__, format_msg, __VA_ARGS__);
+#define log_err_f(format_msg, ...); log_msg_f_detailed("ERROR: ", __FILE__, __LINE__, format_msg, __VA_ARGS__);
 
 // initialize logging
 int log_init();
@@ -11,9 +14,9 @@ int log_init();
 int log_terminate();
 
 // log an error
-int log_msg_detailed(char* file, int line, char* msg);
+int log_msg_detailed(char* err, char* file, int line, char* msg);
 
 // log a formatted message printf style
-int log_msg_f_detailed(int line, char* file, char* format, ...);
+int log_msg_f_detailed(char* err, char* file, int line, char* format, ...);
 
 #endif
