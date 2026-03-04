1ccfb734f10a9fd28b8ca578cc45bb74c05d6b72
Ryan Chou <git@rchou.org>
2026-03-03 21:32:12 -0800

    add comments in header file

--- include/cli.h
+++ include/cli.h
@@ -1,6 +1,10 @@
 #ifndef CLI_H
 #define CLI_H
 
+// Returns:
+//   -1 = no CLI action; caller should continue into TUI
+//    0 = handled successfully; caller should exit(0)
+//    2 = usage error; caller should exit(2)
 int handle_args(int argc, char **argv);
 
 #endif
