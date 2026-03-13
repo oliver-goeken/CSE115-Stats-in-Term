7b2f75ec3e69a972643bec379bc50afaf36947fc
Zachary Roberson <ztrobers@ucsc.edu>
2026-03-12 22:40:32 -0700

    Presentation quality of life (spotidy metadata string and 1500 history limit)

--- src/panel.c
+++ src/panel.c
@@ -382,7 +382,8 @@ static void draw_song_info(const char* track, const char* album, const char* art
     free(track_uri);
 
     if (rc == SPOTIFY_API_ERR_NO_TOKEN) {
-        display_new_text_content_node(g_info_window, "Spotify metadata: set SPOTIFY_ACCESS_TOKEN");
+        //display_new_text_content_node(g_info_window, "Spotify metadata: set SPOTIFY_ACCESS_TOKEN");
+        display_new_text_content_node(g_info_window, "");
         return;
     }
 
